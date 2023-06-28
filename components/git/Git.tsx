51e72ca694fda1695f5a5baffbbb0737fb9aaea3
Chris Thapa <88536680+riddhasoftchris@users.noreply.github.com>
2023-06-28 13:53:28 +0545

    changes in label

--- components/git/Git.tsx
+++ components/git/Git.tsx
@@ -6,7 +6,7 @@ const Github = () => {
         <div id="about" className="flex items-center justify-center h-screen p-40 bg-gradient-to-r from-indigo-500 to-purple-500">
             <div className="flex flex-col items-center justify-center gap-8 p-8 bg-white rounded-2xl">
                 <h1 className="mt-5 text-5xl project-heading" style={{ paddingBottom: "20px" }}>
-                    Days I <strong className="purple">Code</strong>
+                    Days I <strong className="purple">Enjoy Coding </strong>
                 </h1>
                 <GitHubCalendar
                     username="riddhasoftchris"
