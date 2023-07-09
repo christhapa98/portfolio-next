ed3dbb26769fb7b4eee188104704d90246dd463f
riddhasoftchris <88536680+riddhasoftchris@users.noreply.github.com>
2023-07-10 00:24:40 +0545

    fixed build issue

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { projects } from '@/constants/projexts'
-import { Carousel } from 'react-responsive-carousel'
+import Carousel from 'react-material-ui-carousel'
 
 export default function Projects() {
   return (
     <div id="projects" className="flex items-center justify-center h-screen p-40 bg-gradient-to-r from-indigo-500 to-purple-500">
       <Carousel>
-        {projects.map((item, i) => <div className='p-5 transition-all shadow opacity-50 cursor-pointer hover:bg-white hover:opacity-100 rounded-xl hover:-translate-y-6' >
+        {projects.map((item, i) => <div className='p-5 mt-8 transition-all shadow opacity-50 cursor-pointer h-100 hover:bg-white hover:opacity-100 rounded-xl hover:-translate-y-6' key={i}>
           <h2 className='font-bold '>{item.title}</h2>
-          <img className='w-40 rounded-full shadow' src='https://play-lh.googleusercontent.com/91yKQuGhCHRhrQ0QaIGrBZT4gaWBmrR3yn_x4PbEVmpM0jdLceVV9DbopB32Ka7ujIk=w240-h480-rw' />
+          {/* <img className='w-40 rounded-full shadow' src='https://play-lh.googleusercontent.com/91yKQuGhCHRhrQ0QaIGrBZT4gaWBmrR3yn_x4PbEVmpM0jdLceVV9DbopB32Ka7ujIk=w240-h480-rw' /> */}
         </div>)}
       </Carousel>
     </div>
